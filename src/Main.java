04ad5d00a13f8055ea9c018169cee90e39b849d4
KENCHku <kench.mkk3@gmail.com>
2020-10-23 09:42:26 +0600

    hw3

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(20000.0);
+        bankAccount.withDraw(6000);
+    }
+    }
+
